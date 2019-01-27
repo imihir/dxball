1f85f81754a23f5af78c6ada90499ba0c063f1b7
mihir <kalemihir0@gmail.com>
2019-01-27 19:09:23 +0530

    corner hit left

--- dxball.py
+++ dxball.py
@@ -36,16 +36,17 @@ class paddle:
             self.centerx=self.centerx+10    
 class ball:
     def __init__(self,x,y):
-        self.radius = 50;
+        self.width = 25
+        self.height = 25
         self.centerx = x
         self.centery = y
-        self.speed = 5
+        self.speed = 6
         self.direction = randrange(-45, 45)
         if randrange(2) == 0:
             self.direction += 180
-        pygame.draw.circle(screen,white,(self.centerx,self.centery),10)        
+        pygame.draw.rect(screen, white, [self.centerx, self.centery, self.width, self.height])            
     def show(self):
-        pygame.draw.circle(screen,white,(int(self.centerx),int(self.centery)),10)   
+        pygame.draw.rect(screen, white, [self.centerx, self.centery, self.width, self.height])    
     def move(self):
         direction_radians = math.radians(self.direction)
         self.centerx += self.speed * math.cos(direction_radians)
@@ -58,29 +59,58 @@ class ball:
         if self.centery>page_height :
             return 0     
     def collision_b(self,b):
-        a = (self.centerx - b.centerx) 
-        b1 = (self.centery - b.centery)
-        c = math.sqrt((a*a) + (b1*b1))
-        print(c)
-        if c<47:
-            return True
-        return False
+        xt = self.centerx 
+        yt = self.centery 
+        xb = self.centerx + self.width
+        yb = self.centery + self.height
+        xtr = b.centerx 
+        ytr = b.centery 
+        xbr = b.centerx + b.width
+        ybr = b.centery + b.height
+        return (xt < xbr) and (xtr < xb) and (yt < ybr) and (ytr < yb)      
     def bounce(self):
-        self.direction = (360 - self.direction) %360   
+         self.direction = (360 - self.direction) %360   
 class brick:
     def __init__(self,x,y):
-        self.width = 100;
-        self.height = 100;
-        self.centerx=x;
-        self.centery=y;
+        self.width = 70
+        self.height = 25
+        self.centerx=x
+        self.centery=y
+        self.exist=True
+    def show(self):
+        if self.exist:    
+            pygame.draw.rect(screen, white, [self.centerx, self.centery, self.width, self.height])    
+    def collision(self,b):
+        xt = self.centerx 
+        yt = self.centery 
+        xb = self.centerx + self.width
+        yb = self.centery + self.height
+        xtr = b.centerx 
+        ytr = b.centery 
+        xbr = b.centerx + b.width
+        ybr = b.centery + b.height
+        if (xt < xbr) and (xtr < xb) and (yt < ybr) and (ytr < yb) and self.exist:
+            self.exist=False   
+            return True
+        return False
 def play():
     paddlep = paddle(500,450)
-    dball = ball(500,250)
+    dball = ball(500,10)
     paddlep.show()
+    bricks = [];
+    score = 0
+    for i in range(0,15):
+        b = brick((i*73),20)
+        bricks.append(b)
+    for i in range(0,15):
+        b = brick((i*73),50)
+        bricks.append(b)    
     dball.show()
     while True:
         clock.tick()
         screen.fill(black)
+        for i in bricks:
+            i.show()
         scor = dball.move()
         if scor==0:
             break;
@@ -96,8 +126,16 @@ def play():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_d:
                 paddlep.mover()
-        if dball.collision_b(paddlep) :  
+        for i in bricks:
+            if i.collision(dball):
+                dball.bounce()
+                score += 1
+        if dball.collision_b(paddlep):
             dball.bounce()
         paddlep.show()
-        pygame.display.update()         
-play()
+        
+        pygame.display.update()    
+        print(score)
+while True:        
+    play()
+    
